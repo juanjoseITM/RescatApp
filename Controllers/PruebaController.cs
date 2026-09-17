ea36ee33d86d3136785a702eafb94e2cfbdb7ea1
Juan jose Correa rivera <juanjose.correar20@gmail.com>
2026-09-17 09:49:56 -0500

    Conectamos con la base de datos

---
+++ Controllers/PruebaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RescatApp.Repositories;
+
+namespace RescatApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PruebaController : ControllerBase
+    {   
+        private readonly AppDbContext _context;
+
+        public PruebaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProbarConexion()
+        {
+            bool conectado = await _context.ProbarConexion();
+
+            if (conectado)
+                return Ok("Conexión exitosa con la base de datos.");
+
+            return BadRequest("No se pudo conectar con la base de datos.");
+        }
+    }
+}
---
+++ Repositories/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RescatApp.Repositories
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public async Task<bool> ProbarConexion()
+        {
+            return await Database.CanConnectAsync();
+        }
+
+    }
+}
