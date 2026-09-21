f7f9222e9de96706bcc4961fdad29e61e1391a0a
Juan jose Correa rivera <juanjose.correar20@gmail.com>
2026-09-21 12:50:35 -0500

    Creamos los modelos Foto_Mascota, Mascota, Notificacion, Solicitud_Adopcion, Vacuna.

---
+++ Models/Foto_Mascota.cs
@@ -0,0 +1,13 @@
+﻿namespace RescatApp.Models
+{
+    public class Foto_Mascota
+    {
+
+        public int id_foto { get; set; }
+        public string? utl_foto { get; set; }
+        public int orden { get; set; }
+        public int id_mascota { get; set; }
+        public Mascota? Mascota { get; set; }
+
+    }
+}
---
+++ Models/Solicitud_Adopcion.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RescatApp.Models
+{
+    public class Solicitud_Adopcion
+    {
+
+        [Key]
+        public int id_solicitud { get; set; }
+        public DateTime? fecha_solicitud { get; set; }
+        public string? motivo { get; set; }
+        public int id_estado_solicitud { get; set; }
+        public Estado_Solicitud? Estado_Solicitud { get; set; }
+        public DateTime? fecha_respuesta { get; set; }
+        public string? observaciones_rescatista { get; set; }
+        public int id_mascota { get; set; }
+        public Mascota? Mascota { get; set; }
+        public int id_usuario { get; set; }
+        public Usuario? Usuario { get; set; }
+
+
+    }
+}
