178454c05a0899a194a57de4a68573ccc5ec58c2
Mariajosé Restrepo <mariajoserestrepo1131933@correo.itm.edu.co>
2026-09-25 15:39:14 -0500

    Implementación de arquitectura en n-capas para Seguimientos

---
+++ Models/Refugio.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RescatApp.Models
+{
+    public class Refugio
+    {
+        [Key]
+        public int id_refugio { get; set; }
+        public string? nombre_refugio { get; set; }
+        public string? direccion { get; set; }
+        public string? telefono { get; set; }
+        public string? descripcion { get; set; }
+    }
+}
