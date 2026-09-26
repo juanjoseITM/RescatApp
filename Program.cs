6e9bf8fbfdae7caae8aa1084f3424af6e86e47c2
dloaiza1999 <davidloaiza1124597@correo.itm.edu.co>
2026-09-25 22:41:37 -0500

    Configure Swagger for API documentation
    Added Swagger configuration for API documentation and enabled it in development.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using RescatApp.Repositories;
+using RescatApp.Services; //Service
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Configuración de OpenAPI / Swagger
+builder.Services.AddEndpointsApiExplorer();
+
+// Agrega Swagger UI interactivo
+builder.Services.AddSwaggerGen(); 
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -13,12 +20,18 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("ConexionSQL")));
 
+//Registro de Inyección de Dependencias para el módulo Mascotas
+builder.Services.AddScoped<MascotasRepository>();
+builder.Services.AddScoped<MascotasServices>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // Habilita la interfaz visual de Swagger
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
